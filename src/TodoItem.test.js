3e7a8a1b04524de252a1355d213479948203c107
Boyaa <cbh0548@icloud.com>
2022-10-17 16:29:15 +0900

    TodoItem done style test ok

--- src/TodoItem.test.js
+++ src/TodoItem.test.js
@@ -26,5 +26,13 @@ describe('<TodoItem />', () => {
         expect(span).toBeTruthy(); // span 구성요소가 있니
         expect(button).toBeTruthy(); // button 구성 요소가 있니 
     });
+    it('show line-through on span when done is true', () => {
+        const {span} = setup({todo: {...sampleTodo, done: true} });
+        expect(span).toHaveStyle('text-decoration: line-through;');
+    });
+    it('show line-through on span when done is false', () => {
+        const {span} = setup({todo: {...sampleTodo, done: false} });
+        expect(span).not.toHaveStyle('text-decoration: line-through;');
+    });
  
 });
